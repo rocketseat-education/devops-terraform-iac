11199793863d48b3d9a677b44197dc1effbe23d1
EgydioBNeto <egydiobolonhezi@gmail.com>
2024-05-14 20:17:36 -0300

    Configurando roles

--- iam.tf
+++ iam.tf
@@ -12,3 +12,29 @@ resource "aws_iam_openid_connect_provider" "oidc-git" {
     IAC = "True"
   }
 }
+
+resource "aws_iam_role" "ecr-role" {
+  name = "ecr-role"
+
+  assume_role_policy = jsonencode({
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub" = "repo:eusouodaniel/rocketseat.ci.api:ref:refs/heads/main"
+          }
+        }
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::403429280851:oidc-provider/token.actions.githubusercontent.com"
+        }
+      }
+    ]
+    Version = "2012-10-17"
+  })
+  tags = {
+    IAC = "True"
+  }
+}
