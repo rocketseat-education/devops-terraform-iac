eae0ed1042e4be7c8ee469b865b67ebeba10220a
EgydioBNeto <egydiobolonhezi@gmail.com>
2024-05-29 09:43:30 -0300

    Configurando serviço que vai rodar o container

--- iam.tf
+++ iam.tf
@@ -13,6 +13,29 @@ resource "aws_iam_openid_connect_provider" "oidc-git" {
   }
 }
 
+resource "aws_iam_role" "app-runner-role" {
+  name = app-runner-role
+
+  assume_role_policy = jsondecode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          "Service" = "build.apprunner.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+  ]
+  tags = {
+    IAC = "True"
+  }
+}
+
 resource "aws_iam_role" "ecr-role" {
   name = "ecr-role"
 
@@ -38,9 +61,23 @@ resource "aws_iam_role" "ecr-role" {
   inline_policy {
     name = "ecr-app-permissions"
     policy = jsonencode({
-      Statement = [
+      Statement = [{
+        Sid      = "Statement1"
+        Action   = "apprunner:*"
+        Effect   = "Allow"
+        Resource = "*"
+        },
         {
-          Sid = "Statement1"
+          Sid = "Statement2"
+          Action = [
+            "iam:PassRole",
+            "iam:CreateServiceLinkedRole",
+          ]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+        {
+          Sid = "Statement3"
           Action = [
             "ecr:GetDownloadUrlForLayer",
             "ecr:BatchGetImage",
