d802e13776c69670500524681c9e891bd7e2cdc0
EgydioBNeto <egydiobolonhezi@gmail.com>
2024-05-29 11:56:15 -0300

    Configurando permissões

--- ecr.tf
+++ ecr.tf
@@ -1,12 +1,12 @@
 resource "aws_ecr_repository" "rocketseat-ci-api" {
-    name = "rocketseat-ci"
-    image_tag_mutability = "MUTABLE"
+  name                 = "rocketseat-ci"
+  image_tag_mutability = "MUTABLE"
 
-    image_scanning_configuration {
-        scan_on_push = true
-    }
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 
-    tags = {
-        IAC = "True"
-    }
+  tags = {
+    IAC = "True"
+  }
 }
--- iam.tf
+++ iam.tf
@@ -13,6 +13,32 @@ resource "aws_iam_openid_connect_provider" "oidc-git" {
   }
 }
 
+resource "aws_iam_role" "tf-role" {
+  name = "tf-role"
+
+  assume_role_policy = jsonencode({
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub" = "repo:eusouodaniel/rocketseat.ci.iac:ref:refs/heads/main"
+          }
+        }
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::403429280851:oidc-provider/token.actions.githubusercontent.com"
+        }
+      }
+    ]
+    Version = "2012-10-17"
+  })
+  tags = {
+    IAC = "True"
+  }
+}
+
 resource "aws_iam_role" "app-runner-role" {
   name = app-runner-role
 
